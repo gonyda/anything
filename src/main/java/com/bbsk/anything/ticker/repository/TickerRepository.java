bb1b6c80f474574f544edc008dcc8ad4ce70231c
gony <bbsk3939@gmail.com>
2025-01-12 16:10:42 +0900

    티커 조회 화면개발

--- src/main/java/com/bbsk/anything/ticker/repository/TickerRepository.java
+++ src/main/java/com/bbsk/anything/ticker/repository/TickerRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 public interface TickerRepository extends JpaRepository<Ticker, Long> {
 
-    // LIKE 검색 함수 정의
     List<Ticker> findTop10ByTickerNameContainingOrderByTickerId(String ticker);
 }
